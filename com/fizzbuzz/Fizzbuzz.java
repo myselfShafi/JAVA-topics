ec4803142f20680d0cdd6fff3d7d55c51fc0ec28
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-03-12 20:02:00 +0530

    completed- mortgage, fizzbuzz

--- com/fizzbuzz/Fizzbuzz.java
+++ com/fizzbuzz/Fizzbuzz.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 public class Fizzbuzz {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter number: ");
-            int number = scanner.nextInt();
+            while (true) {
+                System.out.println("Enter number(To exit enter 0): ");
+                int number = scanner.nextInt();
 
-            if ((number % 5) == 0)
-                System.out.println("Fizz");
-            else if ((number % 3) == 0)
-                System.out.println("Buzz");
-            else
-                System.out.println(number);
+                if (number == 0)
+                    break;
+
+                if ((number % 5) == 0 && (number % 3) == 0)
+                    System.out.println("FizzBuzz");
+                else if ((number % 5) == 0)
+                    System.out.println("Fizz");
+                else if ((number % 3) == 0)
+                    System.out.println("Buzz");
+                else
+                    System.out.println("Number: " + number);
+            }
         }
     }
 }
--- com/mortgage/Mortgage.java
+++ com/mortgage/Mortgage.java
@@ -4,27 +4,66 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Mortgage {
-    public static void main(Number[] args) {
+    private static final String RESET = "\u001B[0m";
+    private static final String RED = "\u001B[31m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+
+    public static void main(String[] args) {
         final byte MONTHS_IN_YEARS = 12;
         final byte PERCENT = 100;
 
+        boolean isPrincipleEntered = false;
+        boolean isInterestEntered = false;
+
         try (Scanner scanner = new Scanner(System.in)) { // try-with-resources block
-            System.out.print("Principle: ");
-            int principle = scanner.nextInt();
+            int principle = 0;
+            float monthlyInterestRate = 0;
 
-            System.out.print("Annual Interest Rate: ");
-            float annualInterestRate = scanner.nextFloat();
-            float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEARS;
+            while (true) {
+                if (!isPrincipleEntered) {
+                    System.out.print("Principle ($1K - $100K): ");
+                    principle = scanner.nextInt();
 
-            System.out.print("Periods(Years): ");
-            byte years = scanner.nextByte();
-            int numberOfPayments = years * MONTHS_IN_YEARS;
+                    if (principle < 1000 || principle > 100_000) {
+                        warn("Principle amount must be between $1K TO $100K!");
+                        continue;
+                    }
+                    isPrincipleEntered = true;
+                }
 
-            double mortgage = principle * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
-                    / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+                if (!isInterestEntered) {
+                    System.out.print("Annual Interest Rate (1 - 30): ");
+                    float annualInterestRate = scanner.nextFloat();
 
-            String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-            System.out.println("Mortgage: " + formattedMortgage);
+                    if (annualInterestRate < 1 || annualInterestRate > 30) {
+                        warn("Annual interest rate to be between 1 TO 30!");
+                        continue;
+                    }
+                    monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEARS;
+                    isInterestEntered = true;
+                }
+
+                System.out.print("Period(Years)(1 - 30): ");
+                byte years = scanner.nextByte();
+                if (years < 1 || years > 30) {
+                    warn("Period to be between 1 TO 30 years!");
+                    continue;
+                }
+                int numberOfPayments = years * MONTHS_IN_YEARS;
+
+                double mortgage = principle
+                        * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
+                        / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+
+                String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+                System.out.println(GREEN + "Mortgage: " + formattedMortgage + RESET);
+                break;
+            }
         }
+    }
+
+    private static void warn(String text) {
+        System.out.println(RED + "Warning: " + RESET + YELLOW + text + RESET);
     }
 }
