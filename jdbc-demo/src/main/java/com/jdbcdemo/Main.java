3118a67fcf03a74d73bfbf37572c8394c2a1b0f0
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-09-14 22:35:06 +0530

    jdbc crud-ops

--- jdbc-demo/src/main/java/com/jdbcdemo/Main.java
+++ jdbc-demo/src/main/java/com/jdbcdemo/Main.java
@@ -21,23 +21,33 @@ public class Main {
         String url = "jdbc:postgres://localhost:3000/demo";
         String uname = "postgres";
         String pwd = "0000";
+        
         String query = "select * from students";
+        
+        String createQuery = "insert into student values (5, 'John', 48)";
+        String updateQuery = "update student set sname = 'Max' where sid = 5";
+        String deleteQuery = "delete from student where sid = 5";
 
         // Class.forName("org.postgresql.Driver");
 
         // establish connection
         // // try-with-resources so the connection closes automatically, no need of conn.close()
-        try (Connection conn = DriverManager.getConnection(url,uname,pwd);) {
-            System.out.println("Connection established!");
-
+        try (
+            Connection conn = DriverManager.getConnection(url,uname,pwd);
             // create statement
             Statement stmt = conn.createStatement();
-
             // execute statement
             ResultSet rs = stmt.executeQuery(query);
+            ) {
+            System.out.println("Connection established!");
+
             while (rs.next()) {
-                System.out.println(rs.getString("username"));
+                System.out.println(rs.getString("username")); // or can also provide columnIndex
             }
+
+            stmt.execute(createQuery);
+            stmt.execute(updateQuery);
+            stmt.execute(deleteQuery);
         } catch (SQLException e) {
             e.printStackTrace();
         }
