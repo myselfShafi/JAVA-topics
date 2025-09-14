3eff9381db9de66f3a2e9822b12870b8e2da8924
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-09-14 22:22:29 +0530

    jdbc execution setup

--- jdbc-demo/src/main/java/com/jdbcdemo/Main.java
+++ jdbc-demo/src/main/java/com/jdbcdemo/Main.java
@@ -2,7 +2,9 @@ package com.jdbcdemo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +21,7 @@ public class Main {
         String url = "jdbc:postgres://localhost:3000/demo";
         String uname = "postgres";
         String pwd = "0000";
+        String query = "select * from students";
 
         // Class.forName("org.postgresql.Driver");
 
@@ -26,8 +29,18 @@ public class Main {
         // // try-with-resources so the connection closes automatically, no need of conn.close()
         try (Connection conn = DriverManager.getConnection(url,uname,pwd);) {
             System.out.println("Connection established!");
+
+            // create statement
+            Statement stmt = conn.createStatement();
+
+            // execute statement
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getString("username"));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
