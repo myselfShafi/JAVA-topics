b916e51fa09538910144ce6a6b32c99906888abf
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-10-19 18:09:41 +0530

    changing column, table names with custom naming. adding a Transient field

--- hibernate-demo/src/main/java/com/hibernatedemo/Student.java
+++ hibernate-demo/src/main/java/com/hibernatedemo/Student.java
@@ -2,13 +2,19 @@ package com.hibernatedemo;
 
 import jakarta.persistence.*;
 
-@Entity
+@Entity                                     // tells Hibernate: “this class should be saved in a db table”
+@Table(name = "student_collection")         // this will rename the db table_name to specified name
 public class Student {
 
-    @Id
+    @Id                                     // primary key
     private int rollNo;
+
+    @Column(name = "student_name")          // this will rename the db table column_name to specified name
     private String name;
     private int age;
+
+    @Transient                              // ignores the below field; doesn’t store it in the database
+    private String address;
 
     public void setRollNo(int rollNo) {
         this.rollNo = rollNo;
@@ -22,6 +28,10 @@ public class Student {
         this.age = age;
     }
 
+    public String setAddress() {
+        return address;
+    }
+
     public int getRollNo() {
         return rollNo;
     }
@@ -32,6 +42,10 @@ public class Student {
 
     public int getAge() {
         return age;
+    }
+
+    public void getAddress(String address) {
+        this.address = address;
     }
 
     @Override
